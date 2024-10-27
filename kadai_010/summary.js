d9d554506d2653438f8dc81b34d2bce05a8fe411
yuki8693 <yuuki19920215@gmail.com>
2024-10-28 02:39:41 +0900

    kadai_010をアップロードしました。

---
+++ kadai_010/summary.js
@@ -0,0 +1,15 @@
+$('#change-color').on('click', ()=> {
+  $('#target').css('color', 'red');
+});
+
+$('#change-text').on('click', ()=> {
+  $('#target').text('Hello!');
+});
+
+$('#fade-out').on('click', ()=> {
+  $('#target').fadeOut();
+});
+
+$('#fade-in').on('click', ()=> {
+  $('#target').fadeIn();
+});
